1b75d1aaa6c9b178156e9e6cc5ae6c6316e8276f
kimnu-yang <devgimun@naver.com>
2023-11-07 14:38:09 +0900

    init project

---
+++ src/main/java/com/teamfresh/voc_manage/VocManageApplication.java
@@ -0,0 +1,13 @@
+package com.teamfresh.voc_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamfresh/voc_manage/VocManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamfresh.voc_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
